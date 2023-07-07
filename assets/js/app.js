554dd6f7b783a7c8b8dbbe3a297f95bea98e14a3
atps0101 <atps.php@gmail.com>
2023-07-07 16:53:59 +0300

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -129,6 +129,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const swiper = new Swiper('.swiper', {
               direction: 'horizontal', // Указали направление горизонтальное
               loop: true,
+              speed: 1000,
               pagination: {
                 el: '.swiper-pagination',
                 clickable: true, // Если хотите, чтобы пагинация была кликабельной
